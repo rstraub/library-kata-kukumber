0e2de493f8ac33b0b9e586124c3b5cdb7f78272f
Roy Straub <roy.straub@codesquad.nl>
2021-04-09 15:42:33 +0200

    Test for retrieving books from inventory

--- src/test/kotlin/nl/codecraftr/kata/librarykukumber/storage/InMemoryBookInventoryTest.kt
+++ src/test/kotlin/nl/codecraftr/kata/librarykukumber/storage/InMemoryBookInventoryTest.kt
@@ -1,29 +1,42 @@
 package nl.codecraftr.kata.librarykukumber.storage
 
 import nl.codecraftr.kata.librarykukumber.listOfBooks
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class InMemoryBookInventoryTest {
     private lateinit var books: List<String>
     private lateinit var results: List<String>
+    private lateinit var inventory: InMemoryBookInventory
 
     @BeforeEach
     internal fun setUp() {
         books = emptyList()
         results = emptyList()
+        inventory = InMemoryBookInventory()
     }
 
-    @Test
-    internal fun `should return all books stored in the inventory`() {
-        books = listOfBooks()
-        val inventory = InMemoryBookInventory(books)
+    @Nested
+    inner class GetBooks {
+        @Test
+        internal fun `should return all books stored in the inventory`() {
+            books = listOfBooks()
+            inventoryWithBooks()
 
+            retrieveBooks()
+
+            retrievedBooksShouldMatchSupplied()
+        }
+    }
+
+    private fun inventoryWithBooks() {
+        inventory = InMemoryBookInventory(books)
+    }
+
+    private fun retrieveBooks() {
         results = inventory.getBooks()
-
-        retrievedBooksShouldMatchSupplied()
     }
 
     private fun retrievedBooksShouldMatchSupplied() {
